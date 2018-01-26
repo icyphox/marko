606ef3309cbb82d1d79ea1f526498441d8d0e2f5
icyphox <icyph0x@protonmail.com>
2018-01-27 02:07:15 +0530

    modularize code

--- bot.py
+++ bot.py
@@ -1,25 +1,13 @@
 import tweepy, os, markovify, time, sys, argparse, requests
 from inscriptis import get_text
+from modules import auth
 from colorama import Fore, init
 
 # colorama autoreset
 init(autoreset=True)
 
-if os.path.isfile('consumer.py'):
-    import consumer
-    auth = tweepy.OAuthHandler(consumer.C_KEY, consumer.C_SECRET) 
-    auth.set_access_token(consumer.A_TOKEN, consumer.A_SECRET)
-else:
-# api keys exported in virtualenv
-    A_TOKEN = os.environ['A_TOKEN']
-    A_SECRET = os.environ['A_SECRET']
-    C_KEY = os.environ['C_KEY']
-    C_SECRET = os.environ['C_SECRET']
-
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-    auth.set_access_token(A_TOKEN, A_SECRET)
-
-api = tweepy.API(auth) 
+# auth
+api = auth.tweepy_auth()
 
 # get file path from args
 parser = argparse.ArgumentParser(description='A Twitter bot that tweets Markov chains.')
