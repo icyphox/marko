bd77620f5ba2bf7878e7cf291db986a0b87b461d
icyphox <icyph0x@protonmail.com>
2018-01-27 01:41:35 +0530

    fix consumer.py import an tweepy auth

--- bot.py
+++ bot.py
@@ -7,15 +7,17 @@ init(autoreset=True)
 
 if os.path.isfile('consumer.py'):
     import consumer
+    auth = tweepy.OAuthHandler(consumer.C_KEY, consumer.C_SECRET) 
+    auth.set_access_token(consumer.A_TOKEN, consumer.A_SECRET)
 else:
 # api keys exported in virtualenv
-    A_TOKEN= os.environ['A_TOKEN']
+    A_TOKEN = os.environ['A_TOKEN']
     A_SECRET = os.environ['A_SECRET']
     C_KEY = os.environ['C_KEY']
     C_SECRET = os.environ['C_SECRET']
 
-auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-auth.set_access_token(A_TOKEN, A_SECRET)
+    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+    auth.set_access_token(A_TOKEN, A_SECRET)
 
 api = tweepy.API(auth) 
 
