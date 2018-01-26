15824003832e879b5623bcacd5be1d47af1ddd2b
icyphox <icyph0x@protonmail.com>
2018-01-26 14:18:43 +0530

    remove sleep and add useless prints

--- bot.py
+++ bot.py
@@ -27,24 +27,28 @@ args = parser.parse_args()
 def gen_textmodel():
     if args.file:
         with open(args.f) as f:
+            print('[+] Parsing text file.')
             text = f.read()
     elif args.url:
         url = args.url
         html = urllib.request.urlopen(url).read().decode('utf-8')
+        print('[+] Parsing specified URL.')
         text = get_text(html)
     model = markovify.Text(text)
+    print('[+] Generating model.')
     return model
 
 def construct_twt(text_model):
     markov_text = text_model.make_short_sentence(140)
+    print('[+] Constructing your tweet.')
     if '`' or '"' not in text_model:
         twt_txt = '[icybot]\n'  + markov_text
     return twt_txt
 
 def do_tweet(text):
     api.update_status(text)
-    time.sleep(1800)
-    
+    print('[!] Tweet sent.')
+
 def main():
     model = gen_textmodel()
     tweet_text = construct_twt(model)
