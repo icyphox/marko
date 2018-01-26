06cad98e9a6402ff4107e3d5bc36d29d35c2e476
icyphox <icyph0x@protonmail.com>
2018-01-26 14:49:35 +0530

    better exception handling and add user-agents to prevent 403s

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ api = tweepy.API(auth)
 
 # get file path from args
 parser = argparse.ArgumentParser(description='A Twitter bot that tweets Markov chains.')
-#group = parser.add_mutually_exclusive_group(required=True)
 parser.add_argument('-f', '--file', help='path to text model (.txt)')
 parser.add_argument('-u', '--url', help='urlto generate a model from')
 
@@ -31,19 +30,27 @@ def gen_textmodel():
             text = f.read()
     elif args.url:
         url = args.url
-        html = urllib.request.urlopen(url).read().decode('utf-8')
         print('[+] Parsing specified URL.')
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+        headers={'User-Agent':user_agent,}
+        request = urllib.request.Request(url,None,headers)
+        html = urllib.request.urlopen(request).read().decode('utf-8')
         text = get_text(html)
     model = markovify.Text(text)
-    print('[+] Generating model.')
+    print('[+] Generating a model.')
     return model
 
 def construct_twt(text_model):
+    print('[+] Attempting to generate a Markov chain.')
     markov_text = text_model.make_short_sentence(140)
-    print('[+] Constructing your tweet.')
-    if '`' or '"' not in text_model:
-        twt_txt = '[icybot]\n'  + markov_text
-    return twt_txt
+    if markov_text == None:
+        print('[!] Failed to generate a Markov chain. Exiting.')
+        sys.exit(1)
+    else:
+        print('[+] Constructing your tweet.')
+        if '`' or '"' not in text_model:
+            twt_txt = '[icybot]\n'  + markov_text
+        return twt_txt
 
 def do_tweet(text):
     api.update_status(text)
