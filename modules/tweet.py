6d96e5c27d86c14ee7b52658faf21badfdd63fc3
icyphox <icyph0x@protonmail.com>
2018-01-29 15:13:41 +0530

    commented out now, back later

--- modules/tweet.py
+++ modules/tweet.py
@@ -1,4 +1,4 @@
-import tweepy, os.path, sys
+import tweepy, os.path, sys, json
 from colorama import Fore, init
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
@@ -13,14 +13,19 @@ def do_tweet(text):
     print(Fore.CYAN + '[*] Tweet can be found at https://twitter.com/' + api.me().screen_name  + '/status/' + tweet.id_str)
 
 def get_history(user):
-    filename = user[1:] + '.json'
-    for status in tweepy.Cursor(api.user_timeline, screen_name=user).items():
-        if not os.path.exists('timelines'):
-            os.makedirs('timelines')
-            with open('timelines'+filename, 'w') as f:
-                f.write(status._json)
-        else:
-            with open('timelines'+filename, 'w') as f:
-                f.write(status._json)
-
-    return 'timelines'+filename
+    filename = user[1:] + '.txt'
+    path = 'timelines/' + filename
+#    for status in tweepy.Cursor(api.user_timeline, screen_name=user).items():
+#         if not os.path.exists('timelines'):
+#            os.makedirs('timelines')
+#            with open(path, 'w') as f:
+#                print(status._json['text'])
+#                f.write(json.dumps(status._json))
+#                f.write('\n')
+#        else:
+#            with open(path, 'w') as f:
+#                print(status._json['text'])
+#                f.write(json.dumps(status._json))
+#                f.write('\n')
+# 
+    return path
