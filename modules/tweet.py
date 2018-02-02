e280014dfb281529b03f91bdbd19a3bb6bab8298
icyphox <icyph0x@protonmail.com>
2018-02-02 13:22:24 +0530

    add primitive progress bar

--- modules/tweet.py
+++ modules/tweet.py
@@ -1,4 +1,5 @@
 import tweepy, os.path, sys, json
+from tqdm import tqdm
 from colorama import Fore, init
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
@@ -11,8 +12,10 @@ api = auth.tweepy_auth()
 def do_tweet(text):
     tweet = api.update_status(text)
     print(Fore.CYAN + '[*] Tweet can be found at https://twitter.com/' + api.me().screen_name  + '/status/' + tweet.id_str)
+    
 
 def get_history(user):
+
     filename = user[1:] + '.txt'
     all_tweets = []
     path = 'timelines/' + filename
@@ -24,7 +27,8 @@ def get_history(user):
         new_tweets = api.user_timeline(screen_name=user,count=200,max_id=oldest)
         all_tweets.extend(new_tweets)
         oldest = all_tweets[-1].id - 1
-        print(Fore.GREEN + '[+] %s tweets downloaded so far' % (len(all_tweets)), end='\r')
+        for i in trange(len(all_tweets)):
+            pass
 
     out_tweets = [tweet.text.encode('utf-8') for tweet in all_tweets]
     
